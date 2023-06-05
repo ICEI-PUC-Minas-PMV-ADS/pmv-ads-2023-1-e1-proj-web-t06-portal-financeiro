393911368af8c985cb1e0762d3313e24789ef787
George Lucas <66649959+GeorgeNeb@users.noreply.github.com>
2023-06-04 22:57:07 -0300

    Update categorias.js

---
+++ Site/js/categorias.js
@@ -0,0 +1,52 @@
+var posicaoVertical = 0;
+var btnAdd = document.querySelector('.bnt-add');
+btnAdd.addEventListener('click', function() {
+  var novaCategoria = document.getElementById('textInput').value;
+  var limiteGasto = document.getElementById('NumberInput').value;
+  var novaDiv = document.createElement('div');
+  novaDiv.classList.add('element');
+     novaDiv.textContent = novaCategoria + ' Limite:R$' + limiteGasto;
+
+     var buttonContainer = document.createElement('div');
+     buttonContainer.classList.add('button-container');
+     buttonContainer.style.marginTop = '14px';
+
+     var editButton = document.createElement('button');
+     editButton.classList.add('edit-btn');
+     editButton.addEventListener('click', function() {
+       //editar a categoria, não faço ideia to pensando XD//
+     });
+   
+     var deleteButton = document.createElement('button');
+     deleteButton.classList.add('delete-btn');
+     deleteButton.addEventListener('click', function() {
+        novaDiv.remove();
+        organizarCategorias();
+     });
+   
+     buttonContainer.appendChild(editButton);
+     buttonContainer.appendChild(deleteButton);
+   
+     novaDiv.appendChild(buttonContainer);
+     
+  var elementContainer = document.getElementById('element-container');
+  novaDiv.style.marginTop = posicaoVertical + 'px';
+  posicaoVertical += 55;
+  elementContainer.appendChild(novaDiv);
+  document.getElementById('textInput').value = '';
+  document.getElementById('NumberInput').value = '';
+
+  organizarCategorias();
+
+
+
+});
+
+function organizarCategorias() {
+    var elementContainer = document.getElementById('element-container');
+    var categorias = elementContainer.querySelectorAll('.element');
+    
+    categorias.forEach(function(categoria, index) {
+      categoria.style.marginTop = index * 50 + 'px';
+    });
+  }
